99af7985052d70c118066d313f8cf5575f2453fa
Marcin Rataj <lidel@lidel.org>
2018-08-07 20:10:44 +0200

    demo: webm fails to load via video tag
    1. open dweb://html/ - it will fail
    2. open dweb://html/video.webm directly - it will play ok
    3. open dweb://html/ again - it will play ok
    4. open dweb://foo/ - it will fail due to different origin

--- demo/protocol/protocol.js
+++ demo/protocol/protocol.js
@@ -1,3 +1,19 @@
+async function* webmDemo() {
+  const encoder = new TextEncoder("utf-8")
+  yield encoder.encode("<h1>HTML</h1>").buffer
+  yield encoder.encode("<p>ContentType was inferred as HTML").buffer
+  yield encoder.encode(
+    `<p>&lt;video src="<span class="aqua">dweb://html/video.webm" /&gt;</p>`
+  ).buffer
+  yield encoder.encode(
+    `<p>
+    <video class="shadow-2 bg-navy-muted" width="640" height="360" style="background-color: #ccc; border: 1px dotted black" autoplay loop muted>
+      <source src="dweb://html/video.webm" type="video/webm">
+    </video>
+    </p>`
+  ).buffer
+}
+
 browser.protocol.registerProtocol("dweb", request => {
   switch (request.url) {
     case "dweb://stream/": {
@@ -24,10 +40,27 @@ browser.protocol.registerProtocol("dweb", request => {
     }
     case "dweb://html/": {
       return {
+        content: webmDemo()
+      }
+    }
+    case "dweb://foo/": {
+      return {
+        content: webmDemo()
+      }
+    }
+    case "dweb://html/video.webm": {
+      return {
         content: (async function*() {
-          const encoder = new TextEncoder("utf-8")
-          yield encoder.encode("<h1>HTML</h1>").buffer
-          yield encoder.encode("<p>ContentType was inferred as HTML").buffer
+          const response = await fetch(
+            "https://ipfs.io/ipfs/bafybeigwa5rlpq42cj3arbw27aprhjezhimhqkvhbb2kztjtdxyhjalr3q/big-buck-bunny_trailer.webm",
+            {
+              mode: "cors",
+              cache: "force-cache"
+            }
+          )
+          console.log("response", response)
+          const payload = await response.arrayBuffer()
+          yield payload
         })()
       }
     }
